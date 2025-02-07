998f3f2833f896b0aea8f16b936f173e4c533394
Laila Khan <lailatariqkhan@gmail.com>
2025-02-07 14:05:09 +0000

    css and javascript exercise for my face

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,14 @@ document.getElementById("changeColor").addEventListener("click", function() {
       list.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
   } 
 });
+
+document.getElementById("sideNavButton").addEventListener("click", function() {
+  console.log('Hello world');
+  document.getElementById("mysidenav").style.width = 300;
+});
+
+ document.getElementById("closeSideNavButton").addEventListener("click", function() {
+   document.getElementById("mysidenav").style.width = 0 ;
+});
+
+
